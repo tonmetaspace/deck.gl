63550838a797e9a5b6a10da236d59daf7ebc3564
Felix Palmer <felixpalmer@gmail.com>
2022-10-26 11:26:59 +0200

    Multisample mask

--- modules/extensions/src/mask/shader-module.ts
+++ modules/extensions/src/mask/shader-module.ts
@@ -14,17 +14,62 @@ const fs = `
 uniform sampler2D mask_texture;
 uniform int mask_channel;
 uniform bool mask_enabled;
+
+float match(vec2 tex, vec3 pickingColor) {
+  vec4 maskColor = texture2D(mask_texture, tex);
+  float delta = distance(maskColor.rgb, pickingColor);
+  float e = 0.000001;
+  return step(delta, e);
+}
+
 float mask_isInBounds(vec2 texCoords, vec3 pickingColor) {
   if (!mask_enabled) {
     return 1.0;
   }
 
+  float O = 0.0;
+  vec2 dx = 2.0 * vec2(1.0 / 2048.0, 0.0);
+  vec2 dy = dx.yx;
   // Visibility test
-  vec4 maskColor = texture2D(mask_texture, texCoords);
-  float delta = distance(maskColor.rgb, pickingColor);
-  float e = 0.000001;
-  return step(delta, e);
 
+  vec2 dd = -2.0 * dy;
+  O += match(texCoords + 2.0 * dx + dd, pickingColor);
+  O += match(texCoords + dx + dd , pickingColor);
+  O += match(texCoords + dd, pickingColor);
+  O += match(texCoords - dx + dd, pickingColor);
+  O += match(texCoords - 2.0 * dx + dd, pickingColor);
+
+  dd = -1.0 * dy;
+  O += match(texCoords + 2.0 * dx + dd, pickingColor);
+  O += match(texCoords + dx + dd , pickingColor);
+  O += match(texCoords + dd, pickingColor);
+  O += match(texCoords - dx + dd, pickingColor);
+  O += match(texCoords - 2.0 * dx + dd, pickingColor);
+
+  dd = 0.0 * dy;
+  O += match(texCoords + 2.0 * dx + dd, pickingColor);
+  O += match(texCoords + dx + dd , pickingColor);
+  O += match(texCoords + dd, pickingColor);
+  O += match(texCoords - dx + dd, pickingColor);
+  O += match(texCoords - 2.0 * dx + dd, pickingColor);
+
+  dd = 1.0 * dy;
+  O += match(texCoords + 2.0 * dx + dd, pickingColor);
+  O += match(texCoords + dx + dd , pickingColor);
+  O += match(texCoords + dd, pickingColor);
+  O += match(texCoords - dx + dd, pickingColor);
+  O += match(texCoords - 2.0 * dx + dd, pickingColor);
+
+  dd = 2.0 * dy;
+  O += match(texCoords + 2.0 * dx + dd, pickingColor);
+  O += match(texCoords + dx + dd , pickingColor);
+  O += match(texCoords + dd, pickingColor);
+  O += match(texCoords - dx + dd, pickingColor);
+  O += match(texCoords - 2.0 * dx + dd, pickingColor);
+
+  O = O / 25.0;
+
+  return pow(O, 2.2);
 
 
   // float maskValue = 1.0;
@@ -57,16 +102,18 @@ varying vec2 mask_texCoords;
   'fs:#decl': `
 varying vec2 mask_texCoords;
 `,
-  'fs:#main-start': `
+  'fs:#main-end': `
   if (mask_enabled) {
     float mask = mask_isInBounds(mask_texCoords, vPickingColor);
 
     // Debug: show extent of render target
     // gl_FragColor = vec4(mask_texCoords, 0.0, 1.0);
-    gl_FragColor = texture2D(mask_texture, mask_texCoords);
-    gl_FragColor.a = mask;
+    // gl_FragColor = texture2D(mask_texture, mask_texCoords);
 
-    if (mask < 0.2) discard;
+    // Fade out mask
+    gl_FragColor.a *= mask;
+
+    if (mask < 0.01) discard;
   }
 `
 };
